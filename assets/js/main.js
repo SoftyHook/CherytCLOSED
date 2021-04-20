e6eb41f18da95ee556918ffe35b47caacd88f596
CherytCode <cherguios8@gmail.com>
2021-04-19 22:19:49 -0500

    New Style, New Vibes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,7 @@
 const btnSwitch = document.querySelector('#switch');
+const btnFilterP = document.querySelector('#filtro-p');
+const btnFilterI = document.querySelector('#filtro-i');
+const btnFilterX = document.querySelector('#filtro-x');
 
 btnSwitch.addEventListener('click', () => {
     document.body.classList.toggle('dark');
@@ -22,3 +25,48 @@ if(localStorage.getItem('dark-mode') === 'true'){
     document.body.classList.remove('dark');
     btnSwitch.classList.remove('active');
 };
+
+btnFilterP.addEventListener('click', () => {
+    document.body.classList.add('selectedP');
+    
+    if(document.body.classList.contains('selectedP')){
+        document.body.classList.remove('selectedI', 'selectedX')
+    }
+
+    btnFilterP.classList.add('focus');
+    
+    if(btnFilterP.classList.contains('focus')){
+        btnFilterI.classList.remove('focus');
+        btnFilterX.classList.remove('focus');
+    };
+});
+
+btnFilterI.addEventListener('click', () => {
+    document.body.classList.add('selectedI');
+
+    if(document.body.classList.contains('selectedI')){
+        document.body.classList.remove('selectedX', 'selectedP')
+    }
+    
+    btnFilterI.classList.add('focus');
+
+    if(btnFilterI.classList.contains('focus')){
+        btnFilterP.classList.remove('focus');
+        btnFilterX.classList.remove('focus');
+    };
+});
+
+btnFilterX.addEventListener('click', () => {
+    document.body.classList.add('selectedX');
+
+    if(document.body.classList.contains('selectedX')){
+        document.body.classList.remove('selectedI', 'selectedP');
+    };
+
+    btnFilterX.classList.add('focus');
+
+    if(btnFilterX.classList.contains('focus')){
+        btnFilterP.classList.remove('focus');
+        btnFilterI.classList.remove('focus');
+    };
+});
